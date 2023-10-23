c30577d5eb42c5332ee4ac97e6a97ca04d04fe26
Dennis Brown <114094178+Flapes32@users.noreply.github.com>
2023-10-23 11:21:47 +0300

    Update zadanie1-2.c

--- zadanie1-2.c
+++ zadanie1-2.c
@@ -12,21 +12,13 @@ int main(int argc, char** argv)
 
 	ZeroMemory(&procsinformation, sizeof(procsinformation));
 
-	if (argc != 2)
-	{
-		printf("Usage: %s [cmdline]\n", argv[0]);
-		return;
-	}
-
-	if (!CreateProcess("C:\\Users\\flapes32\\source\\repos\\Zadanie1\\x64\\Debug\\Zadanie1.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &startupinfo, &procsinformation))
+	if (!CreateProcess(L"C:\\Users\\Asus\\source\\repos\\Zadanie1\\x64\\Debug\\TASK1.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &supinfo, &procsinformation))
 	{
 		printf("CreateProcess failed (%d).\n", GetLastError());
 	}
 
 	WaitForSingleObject(procsinformation.hProcess, INFINITE);
 
-	CHandle(procsinformation.hProcess);
-	CHandle(procsinformation.hThread);
 
 	return 0;
 }
